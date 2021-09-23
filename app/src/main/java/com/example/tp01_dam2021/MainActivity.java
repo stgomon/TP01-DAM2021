8f987f5cd9ded7536b68e3878a6a299099b9f5ce
stgomon <santiagomontalvo95@gmail.com>
2021-09-23 18:47:48 -0300

    Initial commit

---
+++ app/src/main/java/com/example/tp01_dam2021/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tp01_dam2021;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
