9ff53ee5b580ba8605ce000cd0c6023dd4b1fdb4
stgomon <santiagomontalvo95@gmail.com>
2021-09-24 22:34:01 -0300

    En desarrollo primera parte

--- app/src/main/java/com/example/tp01_dam2021/MainActivity.java
+++ app/src/main/java/com/example/tp01_dam2021/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.tp01_dam2021;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final String[] opciones = {"INDUMENTARIA","ELECTRÓNICA","ENTRETENIMIENTO","JARDÍN","VEHÍCULOS","JUGUETES"}
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,opciones);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+       // Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        //spinner.setAdapter(adapter);
     }
 }
